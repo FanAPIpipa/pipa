959969cefaf445c2577aaf0d6b16eff465c8f854
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-03 17:47:28 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -3,5 +3,14 @@ $fgapeScriptUrl = "https://github.com/FanAPIpipa/pipa/raw/main/fgape.ps1"
 $fgapeScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "fgape.ps1"
 
 Invoke-WebRequest -Uri $fgapeScriptUrl -OutFile $fgapeScriptPath
-Start-Process -FilePath "powershell.exe" -ArgumentList "-File $fgapeScriptPath" -Verb RunAs -Wait
- 
+
+# Проверяем, успешно ли был выполнен скрипт fgape.ps1
+if (Test-Path $fgapeScriptPath) {
+    # Запускаем скрипт fgape.ps1
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-File $fgapeScriptPath" -Verb RunAs -Wait
+
+    # Выводим окно с информацией
+    [System.Windows.Forms.MessageBox]::Show("Скрипт fgape.ps1 успешно выполнен.", "Информация")
+} else {
+    Write-Host "Не удалось загрузить скрипт fgape.ps1."
+}
