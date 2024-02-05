60e8fe025a7c7ba5e4c4bd3c61515749385c5bd9
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-05 12:29:45 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -6,14 +6,14 @@ if (Test-Path $fgapeScriptPath) {
     $tempFolder = New-Item -ItemType Directory -Path "$env:TEMP\ProvisionWindows" -Force
     $installPythonBatUrl = "https://raw.githubusercontent.com/nuket/provision-windows/master/install-python.bat"
     $installPythonBatPath = Join-Path -Path $tempFolder.FullName -ChildPath "install-python.bat"
-    $batFileDisabler = "https://raw.githubusercontent.com/FanAPIpipa/pipa/main/disable.bat"
-    $batFileDisablerPath = Join-Path -Path $tempFolder.FullName -ChildPath "disable.bat"
+    #$batFileDisabler = "https://raw.githubusercontent.com/FanAPIpipa/pipa/main/disable.bat"
+    #$batFileDisablerPath = Join-Path -Path $tempFolder.FullName -ChildPath "disable.bat"
     $installPythonToolsBatUrl = "https://raw.githubusercontent.com/nuket/provision-windows/master/install-python-tools.bat"
     $installPythonToolsBatPath = Join-Path -Path $tempFolder.FullName -ChildPath "install-python-tools.bat"
     $batFileUrl = "https://raw.githubusercontent.com/nuket/provision-windows/master/install-python.bat"
     $batFilePath = Join-Path -Path $tempFolder.FullName -ChildPath "install-python.bat"
     Invoke-WebRequest -Uri $installPythonBatUrl -OutFile $installPythonBatPath
-    Invoke-WebRequest -Uri $batFileDisabler -OutFile $batFileDisablerPath
+    #Invoke-WebRequest -Uri $batFileDisabler -OutFile $batFileDisablerPath
     Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonBatPath" -Verb -Wait
     Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonBatPath" -Verb -Wait
     Invoke-WebRequest -Uri $installPythonToolsBatUrl -OutFile $installPythonToolsBatPath
