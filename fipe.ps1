af0b8cfd840c2c1f905a5defca8cac336116b9eb
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-07 11:03:24 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -1,12 +1,8 @@
 $tempFolder = New-Item -ItemType Directory -Path "$env:TEMP\ProvisionWindows" -Force
-$fgapeScriptUrl = "https://github.com/FanAPIpipa/pipa/raw/main/fgape.ps1"
-$fgapeScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "fgape.ps1"
-Invoke-WebRequest -Uri $fgapeScriptUrl -OutFile $fgapeScriptPath
-Start-Process -FilePath "powershell" -ArgumentList "$fgapeScriptPath" -Verb RunAs
 $installPythonToolsBatUrl = "https://raw.githubusercontent.com/nuket/provision-windows/master/install-python-tools.bat"
 $installPythonToolsBatPath = Join-Path -Path $tempFolder.FullName -ChildPath "install-python-tools.bat"
 Invoke-WebRequest -Uri $installPythonToolsBatUrl -OutFile $installPythonToolsBatPath
-Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonToolsBatPath" -Verb RunAs
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonToolsBatPath" -Verb
 $gribScriptUrl = "https://raw.githubusercontent.com/FanAPIpipa/pipa/main/grib.py"
 $gribScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "grib.py"
 Invoke-WebRequest -Uri $gribScriptUrl -OutFile $gribScriptPath
