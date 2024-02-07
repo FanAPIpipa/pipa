c7ca084928256c1ca2788a7f29631ae6cf87f324
FanAPIpipa <156794064+FanAPIpipa@users.noreply.github.com>
2024-02-07 11:04:29 +0300

    Update fipe.ps1

--- fipe.ps1
+++ fipe.ps1
@@ -1,8 +1,4 @@
 $tempFolder = New-Item -ItemType Directory -Path "$env:TEMP\ProvisionWindows" -Force
-$installPythonToolsBatUrl = "https://raw.githubusercontent.com/nuket/provision-windows/master/install-python-tools.bat"
-$installPythonToolsBatPath = Join-Path -Path $tempFolder.FullName -ChildPath "install-python-tools.bat"
-Invoke-WebRequest -Uri $installPythonToolsBatUrl -OutFile $installPythonToolsBatPath
-Start-Process -FilePath "cmd.exe" -ArgumentList "/c $installPythonToolsBatPath" -Verb
 $gribScriptUrl = "https://raw.githubusercontent.com/FanAPIpipa/pipa/main/grib.py"
 $gribScriptPath = Join-Path -Path $tempFolder.FullName -ChildPath "grib.py"
 Invoke-WebRequest -Uri $gribScriptUrl -OutFile $gribScriptPath
